2bd8927fa92f62af26fa0e0833faf817df34138b
Lewis Moore <lewis.moore2707@gmail.com>
2018-03-19 18:00:53 +0000

    Install Enzyme. Add second test for App component. Add configuration for EnzymeAdapter

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Enzyme from 'enzyme';
+import { shallow, mount, render } from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
 import App from './App';
+
+Enzyme.configure({adapter: new EnzymeAdapter() });
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   div.id = "root"
   ReactDOM.render(<App />, div);;
 });
+
+it ('Renders welcome message', () => {
+  const page = mount(<App />);
+  expect(page.text()).toMatch('Welcome to Tracker')
+})
