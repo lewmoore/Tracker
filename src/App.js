b09d26d32a569d7edf69f330f4b81d4c15aa6f70
Lewis Moore <lewis.moore2707@gmail.com>
2018-03-19 17:44:38 +0000

    Add wlecome message to index page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <p> Welcome to Tracker </p>
       </div>
     );
   }
