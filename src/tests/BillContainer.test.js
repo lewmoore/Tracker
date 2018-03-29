2d6faa1b77532c6c3e7995616343e8a9b89639d9
Lewis Moore <lewis.moore2707@gmail.com>
2018-03-29 15:19:05 +0100

    Add enzyme config and describe to billcontainer tests

--- src/tests/BillContainer.test.js
+++ src/tests/BillContainer.test.js
@@ -5,8 +5,12 @@ import { shallow, mount, render } from 'enzyme';
 import EnzymeAdapter from 'enzyme-adapter-react-16';
 import BillContainer from '../components/BillContainer';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  div.id = "root"
-  ReactDOM.render(<BillContainer />, div);;
-});
+Enzyme.configure({adapter: new EnzymeAdapter() });
+
+describe('BillContainer component', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    div.id = "root"
+    ReactDOM.render(<BillContainer />, div);;
+  });
+})
