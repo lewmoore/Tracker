fcb770b57a73584fa290e7a5d7a70668f0948b58
Lewis Moore <lewis.moore2707@gmail.com>
2018-03-31 20:27:42 +0100

    Add AddBill component with addbill method to billcontainer

--- src/components/BillContainer.js
+++ src/components/BillContainer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import BillList from './BillList';
+import AddBill from './AddBill';
 
 class BillContainer extends React.Component {
   constructor(props) {
@@ -7,14 +8,20 @@ class BillContainer extends React.Component {
 
     this.state = {
       bills: [
-        'Rent',
-        'Netflix'
       ]
     }
+
+    this.addBill = this.addBill.bind(this)
+  }
+  addBill(bill) {
+    this.setState((state) => ({
+      bills: state.bills.concat([bill])
+    }));
   }
   render() {
     return (
       <div>
+      <AddBill addNew={this.addBill} />
         <BillList bills={this.state.bills} />
       </div>
     )
