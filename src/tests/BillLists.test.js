8f7f3918570325a0f807bbb44799f4281b782551
Lewis Moore <lewis.moore2707@gmail.com>
2018-04-04 12:31:08 +0100

    Setup BillList test file

---
+++ src/tests/BillLists.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Enzyme from 'enzyme';
+import { shallow, mount, render } from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
+import BillList from '../components/BillList';
+
+Enzyme.configure({adapter: new EnzymeAdapter() });
