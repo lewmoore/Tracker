dceef4ca503a79916f450572c5409851b32826c2
Lewis Moore <lewis.moore2707@gmail.com>
2018-04-12 09:51:48 +0100

    Alter BillList test to accomadte form text

---
+++ src/components/BillForm.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class BillForm extends React.Component {
+  render() {
+    return (
+      <div>
+        <p> Enter your Bills: </p>
+      </div>
+    )
+  }
+}
+
+export default BillForm;
