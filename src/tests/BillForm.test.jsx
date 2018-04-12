686f0cd1cf04d4e25134defc6e83434c8214bfc8
Lewis Moore <lewis.moore2707@gmail.com>
2018-04-12 10:30:14 +0100

    Remove merge conflict text

--- src/tests/BillForm.test.jsx
+++ src/tests/BillForm.test.jsx
@@ -4,10 +4,8 @@ import Enzyme from 'enzyme';
 import { shallow, mount, render } from 'enzyme';
 import EnzymeAdapter from 'enzyme-adapter-react-16';
 import BillForm from '../components/BillForm';
-<<<<<<< HEAD
+
 const Sinon = require('sinon')
-=======
->>>>>>> 9918a7a3c0d661a91bbd2a7c4c43db179ee77b86
 
 Enzyme.configure({adapter: new EnzymeAdapter() });
 
@@ -16,7 +14,6 @@ describe('BillForm component', () => {
     const wrapper = shallow(
       <BillForm />
     );
-<<<<<<< HEAD
     expect(wrapper.text()).toMatch('Enter your Bills:')
   })
 
@@ -27,8 +24,5 @@ describe('BillForm component', () => {
     );
     wrapper.find('form').simulate('submit')
     expect(handleChangeSpy.calledOnce).toEqual(true)
-=======
-    expect(wrapper.text()).toMatch('Hello')
->>>>>>> 9918a7a3c0d661a91bbd2a7c4c43db179ee77b86
   })
 })
